72ae15523f1aed4f807642789a239cec9c69275a
Juliana Girotto <ads.jjgirotto@gmail.com>
2024-09-05 15:40:41 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/juliana/workshop_mongo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.juliana.workshop_mongo.repositories;
+
+import com.juliana.workshop_mongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
---
+++ src/main/java/com/juliana/workshop_mongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.juliana.workshop_mongo.services;
+
+import com.juliana.workshop_mongo.domain.User;
+import com.juliana.workshop_mongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
