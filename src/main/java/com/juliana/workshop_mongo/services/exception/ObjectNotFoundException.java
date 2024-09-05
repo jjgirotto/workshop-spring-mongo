a80bd9fe914b589e47258cd90ddf3fd7f13b91a1
Juliana Girotto <ads.jjgirotto@gmail.com>
2024-09-05 16:09:31 -0300

    Getting user by id

---
+++ src/main/java/com/juliana/workshop_mongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.juliana.workshop_mongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
