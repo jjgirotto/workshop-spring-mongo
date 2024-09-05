3c387033c6c00177828d0ccfbd305d583e464725
Juliana Girotto <ads.jjgirotto@gmail.com>
2024-09-05 15:54:29 -0300

    DTO pattern

--- src/main/java/com/juliana/workshop_mongo/resources/UserResource.java
+++ src/main/java/com/juliana/workshop_mongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.juliana.workshop_mongo.resources;
 
 import com.juliana.workshop_mongo.domain.User;
+import com.juliana.workshop_mongo.dto.UserDTO;
 import com.juliana.workshop_mongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -16,8 +18,9 @@ public class UserResource {
     @Autowired
     private UserService userService;
     @RequestMapping(method = RequestMethod.GET) //ou @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
