0b06368fe4e5dfab9fd4eae3b35c330c40b058c3
Juliana Girotto <ads.jjgirotto@gmail.com>
2024-09-05 17:38:37 -0300

    AuthorDTO

--- src/main/java/com/juliana/workshop_mongo/config/Instantiation.java
+++ src/main/java/com/juliana/workshop_mongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.juliana.workshop_mongo.config;
 
 import com.juliana.workshop_mongo.domain.Post;
 import com.juliana.workshop_mongo.domain.User;
+import com.juliana.workshop_mongo.dto.AuthorDTO;
 import com.juliana.workshop_mongo.repositories.PostRepository;
 import com.juliana.workshop_mongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,14 +29,17 @@ public class Instantiation implements CommandLineRunner {
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         userRepository.deleteAll();
+        postRepository.deleteAll();
+
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou pra SP, bjo", maria);
-        Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
-        postRepository.saveAll(Arrays.asList(p1, p2));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
--- src/main/java/com/juliana/workshop_mongo/domain/Post.java
+++ src/main/java/com/juliana/workshop_mongo/domain/Post.java
@@ -1,12 +1,13 @@
 package com.juliana.workshop_mongo.domain;
 
+import com.juliana.workshop_mongo.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
-@Document
+
 public class Post implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
@@ -14,13 +15,13 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post() {
 
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -60,11 +61,11 @@ public class Post implements Serializable {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
