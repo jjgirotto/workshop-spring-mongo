316fc427cd5e88322d623db477a5d41d365338d9
Juliana Girotto <ads.jjgirotto@gmail.com>
2024-09-05 18:03:10 -0300

    Adding posts

--- src/main/java/com/juliana/workshop_mongo/domain/Post.java
+++ src/main/java/com/juliana/workshop_mongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.juliana.workshop_mongo.domain;
 
 import com.juliana.workshop_mongo.dto.AuthorDTO;
+import com.juliana.workshop_mongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 public class Post implements Serializable {
@@ -16,6 +19,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
 
@@ -67,6 +71,10 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
     }
 
     @Override
